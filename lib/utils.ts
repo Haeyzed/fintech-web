70289dbefba12a0426c92e316b5e9e6387b619de
Haeyzed <muibi.azeezabolade@gmail.com>
2024-10-26 19:25:52 +0100

    Updated api-client and use-api hook

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import {Locale} from "@/i18n.config";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
