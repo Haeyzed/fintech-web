74849f6c075fc9ebf843b4b8886b0ae69d4167e4
Haeyzed <muibi.azeezabolade@gmail.com>
2024-10-26 16:29:11 +0100

    Updated api-client and use-api hook

--- middlewares/withI18nMiddleware.ts
+++ middlewares/withI18nMiddleware.ts
@@ -1,22 +1,19 @@
 import type { NextFetchEvent, NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { i18n, Locale } from '@/i18n.config';
+import { i18n } from '@/i18n.config';
 import { match as matchLocale } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 import { CustomMiddleware } from './chain';
 
-function getLocale(request: NextRequest): Locale {
+function getLocale(request: NextRequest): string | undefined {
     const negotiatorHeaders: Record<string, string> = {};
     request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-    const locales = i18n.locales;
+    // @ts-expect-error locales are readonly
+    const locales: string[] = i18n.locales;
     const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
 
-    return matchLocale(languages, locales, i18n.defaultLocale) as Locale;
-}
-
-function isValidLocale(locale: string): locale is Locale {
-    return i18n.locales.includes(locale as Locale);
+    return matchLocale(languages, locales, i18n.defaultLocale);
 }
 
 export function withI18nMiddleware(middleware: CustomMiddleware) {
@@ -30,26 +27,18 @@ export function withI18nMiddleware(middleware: CustomMiddleware) {
             locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
         );
 
-        let locale = getLocale(request);
-
         if (pathnameIsMissingLocale) {
-            const redirectURL = new URL(request.url);
-            redirectURL.pathname = `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`;
+            const locale = getLocale(request)
+            const redirectURL = new URL(request.url)
+            if (locale) {
+                redirectURL.pathname = `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`
+            }
 
             // Preserve query parameters
-            redirectURL.search = request.nextUrl.search;
+            redirectURL.search = request.nextUrl.search
 
-            response = NextResponse.redirect(redirectURL.toString());
-        } else {
-            // Extract locale from the pathname if it's present
-            const pathLocale = pathname.split('/')[1];
-            if (isValidLocale(pathLocale)) {
-                locale = pathLocale;
-            }
+            return NextResponse.redirect(redirectURL.toString())
         }
-
-        // Set the X-App-Language header
-        response.headers.set('x-App-Language', locale);
 
         return middleware(request, event, response);
     };
