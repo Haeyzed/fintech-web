469d1c4e930f7be2db5334dfbc91ec0bddf4a42a
Haeyzed <muibi.azeezabolade@gmail.com>
2024-10-29 15:52:51 +0100

    Updated

--- components/nav-actions.tsx
+++ components/nav-actions.tsx
@@ -1,13 +1,10 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import {ModeToggle} from "@/components/mode-toggle";
+import * as React from 'react'
+import { ModeToggle } from '@/components/mode-toggle'
 import LocaleSwitcher from '@/components/locale-switcher'
-import { useAuth } from '@/hooks/use-auth'
-import { Label } from '@/components/ui/label'
 
 export function NavActions() {
-  const { user } = useAuth()
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isOpen, setIsOpen] = React.useState(false)
 
@@ -17,7 +14,6 @@ export function NavActions() {
 
   return (
     <div className="flex items-center gap-2 text-sm">
-      <Label>Balance: {user.balance}</Label>
       <ModeToggle/>
       <LocaleSwitcher/>
     </div>
